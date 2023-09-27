9b1a8b907cce3f682a6d246beb8747827f993216
VitoriaRossi <vitoria.rossi555@gmail.com>
2023-09-27 16:48:35 -0300

    Adiciona a resposta do exercício.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.Clear();
+Console.WriteLine("Não é o que o mundo reserva para você, mas o que você traz para o mundo.");
+Console.WriteLine("- Anne with an E.");
+Console.WriteLine("Pressione uma tecla para continuar...");
+Console.ReadKey();
+
+Console.ForegroundColor = ConsoleColor.Magenta;
+Console.WriteLine("You are a blowfish.");
+Console.WriteLine("- Breaking Bad.");
+Console.WriteLine("Pressione uma tecla para continuar...");
+Console.ReadKey();
+
+Console.ForegroundColor = ConsoleColor.Red;
+Console.WriteLine("Eu não sei você, mas eu de repente estou me sentindo repleta de possibilidades.");
+Console.WriteLine("- Anne with an E.");
+Console.WriteLine("Pressione uma tecla para finalizar...");
+Console.ReadKey();
+
+Console.ResetColor();
+Console.Clear();
